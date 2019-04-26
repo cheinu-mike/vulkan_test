1e9e92dc25da9181cef8cb776567b4c2814190f4
cheinu-mike <cheinu.mike@gmail.com>
2019-04-26 15:07:18 -0600

    added queuefamilies

--- vulkanapp.cpp
+++ vulkanapp.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <vector>
+#include <set>
 
 const std::vector<const char*> validationLayers = {
 	"VK_LAYER_LUNARG_standard_validation"
@@ -16,6 +17,7 @@ const std::vector<const char*> validationLayers = {
 	const bool enableValidationLayers = true;
 #endif
 
+/*
 bool vulkanapp::checkValidationLayerSupport() {
 	uint32_t layerCount;
 	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
@@ -25,8 +27,9 @@ bool vulkanapp::checkValidationLayerSupport() {
 	
 	return false;
 }
+*/
 
-void vulkanapp::initWindow (){
+void vulkanapp::initWindow() {
 	glfwInit();
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
@@ -34,23 +37,17 @@ void vulkanapp::initWindow (){
 	window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 }
 
-void vulkanapp::pickPhysicalDevice(){
-	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-
-	uint32_t deviceCount = 0;
-	vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
-	
-	if (deviceCount == 0){
-		throw std::runtime_error("failed to find any GPUs on this device");
-	}
-
-	std::vector<VkPhysicalDevice> devices(deviceCount);
-	vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
-	
-}
-void vulkanapp::initVulkan (){
+void vulkanapp::initVulkan() {
 	createInstance();
+	createSurface();
 	pickPhysicalDevice();
+	createLogicalDevice();
+}
+
+void vulkanapp::createSurface() {
+	if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create");
+	}
 }
 
 void vulkanapp::mainLoop() {
@@ -61,13 +58,15 @@ void vulkanapp::mainLoop() {
 }
 
 void vulkanapp::cleanup() {
+	vkDestroySurfaceKHR(instance, surface, nullptr);
+	vkDestroyInstance(instance, nullptr);
 	glfwDestroyWindow(window);
 
 	glfwTerminate();
 }
 
 
-void vulkanapp::createInstance(){
+void vulkanapp::createInstance() {
 	VkApplicationInfo appInfo = {};
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	appInfo.pApplicationName = "Hello Triangle";
@@ -95,3 +94,120 @@ void vulkanapp::createInstance(){
 }
 
 
+void vulkanapp::pickPhysicalDevice() {
+
+	uint32_t deviceCount = 0;
+	vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+	
+	if (deviceCount == 0){
+		throw std::runtime_error("failed to find any GPUs on this device");
+	}
+
+	std::vector<VkPhysicalDevice> devices(deviceCount);
+	vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+	
+	for (const auto& device : devices){
+		if(isDeviceSuitable(device)){
+			physicalDevice = device;
+			break;
+		}
+	}
+
+	if(physicalDevice == VK_NULL_HANDLE){
+		throw std::runtime_error("failed to find a suitable GPU");
+	}
+}
+
+void vulkanapp::createLogicalDevice() {
+	QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+	
+	std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+	std::set<uint32_t> uniqueQueueFamilies = {indices.graphicsFamily.value(), indices.presentFamily.value()};
+	
+	float queuePriority = 1.0f;
+
+	for (uint32_t queueFamily : uniqueQueueFamilies) {
+		VkDeviceQueueCreateInfo queueCreateInfo = {};
+		queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            	queueCreateInfo.queueFamilyIndex = queueFamily;
+            	queueCreateInfo.queueCount = 1;
+            	queueCreateInfo.pQueuePriorities = &queuePriority;
+            	queueCreateInfos.push_back(queueCreateInfo);
+	}
+	
+	VkPhysicalDeviceFeatures deviceFeatures = {};
+VkDeviceCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+	createInfo.queueCreateInfoCount= static_cast<uint32_t>(queueCreateInfos.size());
+	createInfo.pQueueCreateInfos= queueCreateInfos.data();
+
+	createInfo.pEnabledFeatures = &deviceFeatures;
+
+	createInfo.enabledExtensionCount = 0;
+
+	if (enableValidationLayers) {
+		createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+		createInfo.ppEnabledLayerNames = validationLayers.data();
+	} else {
+		createInfo.enabledLayerCount = 0;
+	}
+
+	if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS){
+		throw std::runtime_error("failed to find logical device");
+	}
+
+	vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+}
+
+bool vulkanapp::isDeviceSuitable(VkPhysicalDevice device) {
+        QueueFamilyIndices indices = findQueueFamilies(device);
+
+        return indices.isComplete();
+}
+
+QueueFamilyIndices vulkanapp::findQueueFamilies(VkPhysicalDevice device) {
+	QueueFamilyIndices indices;
+
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        int i = 0;
+        for (const auto& queueFamily : queueFamilies) {
+            if (queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                indices.graphicsFamily = i;
+            }
+
+	    VkBool32 presentSupport = false;
+	    vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+
+	    if (queueFamily.queueCount > 0 && presentSupport) {
+		    indices.presentFamily = i;
+	    }
+
+            if (indices.isComplete()) {
+                break;
+            }
+
+            i++;
+        }
+
+        return indices;
+}
+
+std::vector<const char*> vulkanapp::getRequiredExtensions() {
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+        if (enableValidationLayers) {
+            extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        }
+
+        return extensions;
+}
--- vulkanapp.h
+++ vulkanapp.h
@@ -4,6 +4,17 @@
 #include "/usr/include/GLFW/glfw3.h"
 
 #include <iostream>
+#include <optional>
+#include <vector>
+#include <set>
+
+struct QueueFamilyIndices {
+ 	std::optional<uint32_t> graphicsFamily;
+	std::optional<uint32_t> presentFamily;
+	bool isComplete() {
+       		return graphicsFamily.has_value();
+    	}
+};
 
 class vulkanapp {
 public:
@@ -13,19 +24,33 @@ public:
 		mainLoop();
 		cleanup();
 	}
+
 private:
 	const int WIDTH = 800;
 	const int HEIGHT = 600;
 	
 	VkInstance instance;
+	VkSurfaceKHR surface;
+
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	VkDevice device;
+
+	VkQueue graphicsQueue;
+	VkQueue presentQueue;
 
 	GLFWwindow* window;
 
 	void initWindow();
-	void pickPhysicalDevice();
 	void initVulkan();
+	void createSurface();
 	void mainLoop ();
 	void cleanup();
+	void pickPhysicalDevice();
 	void createInstance();
-	bool checkValidationLayerSupport();
+	void createLogicalDevice();
+	//bool checkValidationLayerSupport();
+
+	bool isDeviceSuitable(VkPhysicalDevice device);
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+	std::vector<const char*> getRequiredExtensions();
 };
